d92244fe418c3b6bc18d7a07f38a20414b275181
Jair Synthia Steele <14959333+EpicestGamer@users.noreply.github.com>
2023-10-21 20:20:40 -0600

    Decompiled vanilla script and build script

---
+++ build.bat
@@ -0,0 +1,7 @@
+:: Set this to the folder you have Caprica.exe stored
+set CapricaLocation=..\..\Tools
+
+:: Set this to where you have Starfield's decompiled scripts stored.
+set ScriptsLocation=..\..\Scripts
+
+%CapricaLocation%\Caprica.exe -g starfield -i %ScriptsLocation% -R -o Data\Scripts Data\Scripts 
